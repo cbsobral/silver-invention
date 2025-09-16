2aea15a03982251559764dfca758937d8ed9cd15
geref-tn <232589216+geref-tn@users.noreply.github.com>
2025-09-16 18:10:28 +0200

    create project and first-script

---
+++ first-script.R
@@ -0,0 +1,10 @@
+# install.packages(c("tidyverse", "gapminder", "pacman")) # uncomment if already installed
+pacman::p_load(tidyverse, gapminder)
+
+
+data(gapminder)
+head(gapminder)
+
+gapminder_clean <- gapminder %>% 
+  dplyr::rename(life_exp = lifeExp, gdp_per_cap = gdpPercap) %>% 
+  dplyr::mutate(gdp = pop * gdp_per_cap)
